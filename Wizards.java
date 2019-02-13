1a0b9b7d108b420bb614cda2cfc9d1cad8ae340a
Gwapongli <44989103+Gwapongli@users.noreply.github.com>
2019-02-13 14:07:27 +0800

    Create Wizards.java

---
+++ Wizards.java
@@ -0,0 +1,54 @@
+package RPG;
+
+public class Wizards {
+    public class Wizard extends Character{
+    Dice dice = new Dice();
+    //private int castLightningBolt;
+    //private int castHeal;
+    private int getMaxMagic;
+    
+    public int maxMagic;
+    public int currentMagic;
+    
+    
+    
+    public Wizard(String n, int s, int d, int i){ 
+        super(n,s,d,i);
+    }
+    
+    public int castLightningBolt(){
+        if(this.currentMagic >= 5){
+            return dice.roll() * super.getIntelligence();
+        }
+        else return 0;
+    }
+    
+    
+    public int castHeal(int castHeal){
+        if(this.currentMagic >= 8){
+            return dice.roll()*super.getIntelligence();
+        }
+        else return castHeal;
+    }
+
+    
+    public int getMaxMagic(){            
+        return maxMagic;
+    }
+    
+    public void setMaxMagic(int maxMagic){
+        this.maxMagic = maxMagic;
+    }
+    
+    public int getCurrentMagic(){
+        return currentMagic;
+    }
+    
+    public void setCurrentMagic(int currentMagic){
+        this.currentMagic = currentMagic;
+    }
+    
+    
+}
+
+}
